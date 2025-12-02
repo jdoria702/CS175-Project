285d1c619ca7d185eb5593651d391f4845258ac5
Leland Lee <lelandlee@Lelands-MacBook-Pro.local>
2025-12-01 20:13:06 -0800

    Add MiniGame3 - Clean the Dirt minigame

--- app/src/main/java/edu/sjsu/android/a175project/MiniGame3Activity.java
+++ app/src/main/java/edu/sjsu/android/a175project/MiniGame3Activity.java
@@ -1,44 +1,65 @@
 package edu.sjsu.android.a175project;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import edu.sjsu.android.a175project.ShopManager;
 
 public class MiniGame3Activity extends AppCompatActivity {
 
     private CountDownTimer countDownTimer;
     private boolean gameOver = false;
-    private View timeBar;
 
+    private View timeBar;
+    private ImageView dirtOverlay;
     private long maxTime;
+
+    private float dirtAlpha = 1.0f;   // full dirt
+    private final float CLEAN_AMOUNT = 0.08f;  // SWIPES NEEDED
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minigame3);
 
-        Button tapBtn = findViewById(R.id.btn_win3);
         timeBar = findViewById(R.id.timeBar);
-        TextView characterLabel = findViewById(R.id.characterLabel);
-        characterLabel.setText("Character: " + ShopManager.getSelectedCharacter(this));
-
+        dirtOverlay = findViewById(R.id.dirtOverlay);
         timeBar.setPivotX(0);
+
+        // Set character
+        ImageView characterBody = findViewById(R.id.characterBody);
+        String selected = ShopManager.getSelectedCharacter(this);
+        characterBody.setImageResource(ShopManager.getCharacterDrawable(selected));
 
         maxTime = GameManagerActivity.getTimerDuration();
 
         startCountdown();
 
-        tapBtn.setOnClickListener(v -> {
-            if (!gameOver) {
+        dirtOverlay.setOnTouchListener((v, event) -> {
+            if (!gameOver) handleCleaningGesture(event);
+            return true;
+        });
+    }
+
+    private void handleCleaningGesture(MotionEvent event) {
+        // Clean a little bit with each motion
+        if (event.getAction() == MotionEvent.ACTION_MOVE ||
+                event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            dirtAlpha -= CLEAN_AMOUNT;
+            dirtAlpha = Math.max(0f, dirtAlpha);
+            dirtOverlay.setAlpha(dirtAlpha);
+
+            if (dirtAlpha <= 0f) {
                 gameOver = true;
                 countDownTimer.cancel();
                 minigamePassed();
             }
-        });
+        }
     }
 
     private void startCountdown() {
